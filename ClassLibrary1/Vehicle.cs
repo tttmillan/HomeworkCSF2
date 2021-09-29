80b015a22a80cf0540fb68bbda0b88d247525ad1
Anthony Millan <tttmillan@yahoo.com>
2021-09-29 17:48:15 -0500

    Vechile

---
+++ ClassLibrary1/Vehicle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Vehicle
+    {
+        //Fields
+
+
+        private string _make;
+        private string _model;
+        private int _year;
+        private float _weight;
+
+
+        //Properties
+
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public float Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value > 1000)
+                {
+                    _weight = value;
+                }
+                else
+                {
+                    _weight = 0;
+                }
+            }
+        }
+
+
+        //ctors
+
+            //default ctor
+
+        public Vehicle() { }
+
+            //fullyqualified ctor
+
+        public Vehicle(string make, string model, int year, float weight)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Weight = weight;
+        }
+
+
+        //method
+
+        public override string ToString()
+        {
+            return string.Format("The Weight of a {0} {1} {2} is {3} lbs.", Year, Make, Model, Weight);
+        }
+
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ClassLibrary1;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+            s1.FirstName = "Han";
+            s1.LastName = "Solo";
+            Console.WriteLine(s1);
+
+
+            Vehicle v1 = new Vehicle("Chevrolet", "Bel Air", 1957, 3273);
+            Console.WriteLine(v1);
+
+
         }
     }
 }
